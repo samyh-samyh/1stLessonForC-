1c2b5ca0730561760f727f184f5478fe84322b21
Sergey Sidorov <89048598831s@gmail.com>
2022-12-04 00:25:59 +0300

    Решение третьей задачи

---
+++ 3/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите номер дня недели: ");
+int a = int.Parse(Console.ReadLine());
+if (a==1)
+{
+ Console.WriteLine($"{a}- Понедельник");   
+}
+if (a==2)
+{
+     Console.WriteLine($"{a}- Вторник");  
+}
+if (a==3)
+{
+    Console.WriteLine($"{a}- Среда"); 
+}
+if (a==4)
+{
+    Console.WriteLine($"{a}- Четверг"); 
+}
+if (a==5)
+{
+    Console.WriteLine($"{a}- Пятница"); 
+}
+if (a==6)
+{
+    Console.WriteLine($"{a}- Суббота"); 
+}
+if (a==7)
+{
+    Console.WriteLine($"{a}- Воскресенье"); 
+}
