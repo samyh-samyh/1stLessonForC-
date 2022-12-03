83675959cb4d947e5f636d23bac8dcaf8c4c8e52
Sergey Sidorov <89048598831s@gmail.com>
2022-12-04 00:06:22 +0300

    2d example

---
+++ 2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("a:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("b:");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine($"{a}, {b}");
+if (Math.Pow(b, 2)== a)
+{
+    Console.WriteLine("a является квадратом b");
+}
+else 
+{
+    Console.WriteLine("а не является квадратом b");
+}
