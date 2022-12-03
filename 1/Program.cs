835009dbc53f650f0431c0408032ba37aacdf608
Sergey Sidorov <89048598831s@gmail.com>
2022-12-03 23:34:54 +0300

    way to square numbers

---
+++ 1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Inter any number: ");
+int number=int.Parse(Console.ReadLine());
+int sqr=number*number;
+Console.WriteLine($"Number square= {sqr} for {number}");
+
+int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Квадрат числа {number} равен {sqr1}");
+double number2 = 3.3;
+double sqr2=(Math.Pow(number2, 2));
+Console.WriteLine($"Квадрат числа {number2} равен {Math.Round(sqr2, 2)}");
